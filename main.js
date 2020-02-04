f8ca614118ff20553507f68b73fe235ded348b8b
Nilo Peligro <nilopeligro27@gmail.com>
2020-02-04 22:13:35 +1000

    second commit

---
+++ main.js
@@ -0,0 +1,52 @@
+//variables
+var x = ' Nilo, ';
+document.write(x);
+
+//deffierent types of variables
+var music = ' faint, ';
+document.write(music);
+var song = ' I love music, \" it so nice \" ';
+document.write(song);
+
+//using variables with strings
+var name = ' nilo ';
+var age = 28;
+var address = ' duay ';
+document.write(name + ' is my name and my age is ' + age);
+document.write(name + age + address);
+ 
+//functions 
+function funk () {
+    alert('hello world');
+}
+
+//using parameters with functions
+function eat (y) {
+    alert('i love to' + y);
+}
+eat(' eat ');
+eat(' sleep ');
+eat(' play ');
+
+//functions with multiple parameters
+function manggo(one, two) {
+    document.write(one + 'is sweeter than' + two + '<br />');
+}
+manggo(' chocolate ', ' sugar ');
+manggo(' strawberry ', ' apple ');
+manggo(' pineapple ', ' oranges ');
+
+//the return statement
+function addNumbers(a, b)  {
+    var c = a + b;
+    return c;
+}
+document.write(addNumbers(10, 5));
+//if else statement
+var show = 10;
+var eat = 10;
+    if(show != eat) {
+        document.write(' Correct, ');
+    }else{
+        document.write(' wrong, ');
+    }
